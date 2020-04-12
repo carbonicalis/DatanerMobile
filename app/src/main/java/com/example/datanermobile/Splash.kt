c3d3f741589835969ba8ff373733e84b6c3969e0
root <root@localhost.localdomain>
2020-04-12 16:39:21 -0400

    tela do splash

---
+++ app/src/main/java/com/example/datanermobile/Splash.kt
@@ -0,0 +1,26 @@
+package com.example.datanermobile
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        changeToLogin()
+    }
+    fun changeToLogin(){
+        val intent = Intent(this, loginActivity::class.java)
+
+        Handler().postDelayed({
+            intent.change()
+        }, 2000)
+    }
+    fun Intent.change(){
+        startActivity(this)
+        finish()
+    }
+}
