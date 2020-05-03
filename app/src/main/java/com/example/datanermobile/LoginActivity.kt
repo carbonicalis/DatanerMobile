6da823795bcd07306e2baa8ccd686074583804f7
root <root@localhost.localdomain>
2020-05-03 19:51:01 -0400

    interação entre as telas

--- app/src/main/java/com/example/datanermobile/LoginActivity.kt
+++ app/src/main/java/com/example/datanermobile/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.system.OsConstants
 import android.view.View
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -25,12 +27,16 @@ class LoginActivity : AppCompatActivity() {
 
     fun enviar(v:View){
         val user = edUser.text.toString()
+       // val tela2 = Intent(this, BuildingActivity::class.java)
 
         val editor = preferencias?.edit()
 
         editor?.putString("usuário", user)
 
         editor?.commit()
+        //startActivity(BuildingActivity)
+
+
     }
 
 
