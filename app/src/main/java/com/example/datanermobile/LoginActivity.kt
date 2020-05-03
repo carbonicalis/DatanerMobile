4dbf617caf5dbc10a49b63009d6d785d04827a87
root <root@localhost.localdomain>
2020-05-03 18:15:45 -0400

    dados salvos offline

--- app/src/main/java/com/example/datanermobile/LoginActivity.kt
+++ app/src/main/java/com/example/datanermobile/LoginActivity.kt
@@ -1,25 +1,39 @@
 package com.example.datanermobile
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_login.*
 
 
 class LoginActivity : AppCompatActivity() {
 
+    var preferencias:SharedPreferences? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        preferencias = getPreferences(Context.MODE_PRIVATE)
+
+        val user = preferencias?.getString("usuário", "")
+
+        edUser.setText(user)
 
     }
 
-      fun CredenLogin(v:View){
+    fun enviar(v:View){
+        val user = edUser.text.toString()
 
-          val loginUser = edUser.text.toString()
+        val editor = preferencias?.edit()
 
-      }
+        editor?.putString("usuário", user)
+
+        editor?.commit()
+    }
+
+
 }
 
 
