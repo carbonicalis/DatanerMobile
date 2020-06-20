e96f8110eee60e1d5b7b7abcfaa5f3aa6af5ba66
ArielMKT <arielmagalhaeslira@gmail.com>
2020-06-19 23:20:48 -0300

    feat: starting device screen

---
+++ app/src/main/java/com/example/datanermobile/device/DeviceFragment.kt
@@ -0,0 +1,46 @@
+package com.example.datanermobile.device
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+
+import com.example.datanermobile.R
+import com.example.datanermobile.databinding.DeviceFragmentBinding
+
+class DeviceFragment : Fragment() {
+
+    private lateinit var deviceViewModel: DeviceViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: DeviceFragmentBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.device_fragment,
+            container,
+            false
+        )
+
+        val application = requireNotNull(this.activity).application
+
+        val deviceFactory = DeviceViewModelFactory(application)
+
+        deviceViewModel = ViewModelProvider(
+            this,
+            deviceFactory
+        ).get(DeviceViewModel::class.java)
+
+        binding.deviceViewModel = deviceViewModel
+        binding.lifecycleOwner = this
+
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/datanermobile/device/DeviceViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.datanermobile.device
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class DeviceViewModel(
+    application: Application
+) : AndroidViewModel(application) {
+    // TODO: Implement the ViewModel
+}
