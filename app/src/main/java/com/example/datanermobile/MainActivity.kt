67cc2e3e98a9c446f366f98f995a3b339966a5bb
root <root@localhost.localdomain>
2020-06-22 14:38:57 -0400

    alteração dos arquivos main

--- app/src/main/java/com/example/datanermobile/MainActivity.kt
+++ app/src/main/java/com/example/datanermobile/MainActivity.kt
@@ -3,16 +3,10 @@ package com.example.datanermobile
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import com.example.datanermobile.building.BuildingActivity
 import com.example.datanermobile.device.DeviceActivity
 import com.example.datanermobile.screens.workplace.WorkplaceActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.*
-
-
-
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,19 +25,5 @@ class MainActivity : AppCompatActivity() {
         test_workplace.setOnClickListener {
             startActivity(Intent(this, WorkplaceActivity::class.java))
         }
-
-
-
-    }
-
-    fun mostrarFrag1(v:View){
-        supportFragmentManager.beginTransaction().replace(R.id.frame1, Fragment1()).commit()
-    }
-
-    fun mostrarFrag2(v:View){
-        supportFragmentManager.beginTransaction().replace(R.id.frame1, Fragment2()).commit()
-    }
-    fun mostrarFrag3(v:View){
-        supportFragmentManager.beginTransaction().replace(R.id.frame1, Fragment3()).commit()
     }
 }
