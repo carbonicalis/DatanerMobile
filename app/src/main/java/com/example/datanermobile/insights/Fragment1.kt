b058445a05c29c6dea6c87463ceafe2f102f715e
root <root@localhost.localdomain>
2020-06-26 14:13:37 -0400

    criacao do onclick no fragment1

--- app/src/main/java/com/example/datanermobile/insights/Fragment1.kt
+++ app/src/main/java/com/example/datanermobile/insights/Fragment1.kt
@@ -1,13 +1,14 @@
 package com.example.datanermobile.insights
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.datanermobile.R
-
+import kotlinx.android.synthetic.main.fragment_fragment1.paulista
 
 
 class Fragment1 : Fragment() {
@@ -16,6 +17,10 @@ class Fragment1 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val application = requireNotNull(this.activity).application
+
+        paulista.setOnClickListener { startActivity(Intent(application, Consumos::class.java)) }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fragment1, container, false)
 
@@ -24,7 +29,5 @@ class Fragment1 : Fragment() {
 
 
 
-
-
-
 }
+
